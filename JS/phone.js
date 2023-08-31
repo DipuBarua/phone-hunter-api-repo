71d1b3763748cb2e15c912a43f326730a17e33d8
DipuBarua <you@example.com>
2023-08-31 23:10:25 +0600

    update modal

--- JS/phone.js
+++ JS/phone.js
@@ -1,27 +1,29 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones);
 
   // sowh all btn when card length more than 12
   const showAllContainer = document.getElementById('showAll-container')
-  if (phones.length > 12) {
-    console.log(phones.length);
+  if (phones.length > 12 && !isShowAll) {
+    // console.log(phones.length);
     showAllContainer.classList.remove('hidden');
   }
   else {
     showAllContainer.classList.add('hidden');
   }
 
-  // display (limited) first 12 phone cards
-  phones = phones.slice(0, 12);
+  // display (limited) first 12 phone cards if not clicked to show all btn
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   // step1 >> get the mother container.
   const phoneContainer = document.getElementById('phone-container');
@@ -29,7 +31,7 @@ const displayPhones = phones => {
   phoneContainer.textContent = '';
 
   phones.forEach(phone => {
-    console.log(phone);
+    // console.log(phone);
     // step2 >> create a child div(can add class).
     const phoneCard = document.createElement('div');
     phoneCard.classList = `card w-96 bg-gray-100`;
@@ -39,8 +41,8 @@ const displayPhones = phones => {
         <div class="card-body">
           <h2 class="card-title">${phone.brand}</h2>
           <p>${phone.phone_name}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onClick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -51,22 +53,52 @@ const displayPhones = phones => {
   toggleLoadingSpin(false);
 }
 
+// handle show details btn 
+const handleShowDetails = async (id) => {
+  // console.log('show id:',id);
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone);
+  // console.log(data);
+}
+// show phone details
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById('show-details-phone-name');
+  phoneName.innerText = phone.name;
+
+  const showDetailsContainer = document.getElementById('show-details-container');
+  showDetailsContainer.innerHTML = ` 
+  <div class="flex justify-center"><img src="${phone.image}" alt="phone"></div>
+  <p> <span><b>Storage:</b></span> ${phone.mainFeatures.storage}
+  <p> <span><b>Display size:</b></span> ${phone.mainFeatures.displaySize}
+  <p> <span><b>Chip Set:</b></span> ${phone.mainFeatures.chipSet}
+  <p> <span><b>Memory:</b></span> ${phone.mainFeatures.memory}
+  <p> <span><b>Slug:</b></span> ${phone.slug}
+  <p> <span><b>Brand:</b></span> ${phone.brand}
+  <p> <span><b>ReleaseDate:</b></span> ${phone.releaseDate}
+  <p> <span><b>GPS:</b></span> ${phone.others?.GPS || 'No GPS available'}
+  `
+  show_details_modal.showModal()
+}
+
 // handle search btn
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   // console.log('search here');
   toggleLoadingSpin(true);//call loading-spinner
   const inputField = document.getElementById('input-field');
   const searchText = inputField.value;
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 }
 
 // another handle search btn >> recap
-const handleSearch2 = () => {
-  toggleLoadingSpin(true);//call loading-spinner
-  const searchField = document.getElementById('search-field');
-  const searchText = searchField.value;
-  loadPhone(searchText);
-}
+// const handleSearch2 = () => {
+//   toggleLoadingSpin(true);//call loading-spinner
+//   const searchField = document.getElementById('search-field');
+//   const searchText = searchField.value;
+//   loadPhone(searchText);
+// }
 
 // loading spinner
 const toggleLoadingSpin = (isLoading) => {
@@ -78,3 +110,9 @@ const toggleLoadingSpin = (isLoading) => {
     loadingSpinner.classList.add('hidden');
   }
 }
+
+// handle show all 
+const handleShowAll = () => {
+  handleSearch(true);
+}
+
